606ade7c15cfe5c4aa76d64f6c0d2cce9171701c
petopello <52197415+petopello@users.noreply.github.com>
2025-03-06 22:38:50 +0100

    Update PSA.cpp

--- PSA.cpp
+++ PSA.cpp
@@ -3,68 +3,68 @@
 #include <iostream>
 using namespace std;
 
+//basic transformation, same result as using numpy functions reshape+roll+flatten
+void apply_diff(int shift, int group, const vector<double>& read, vector<double>& write) {
+    int unshift = group - shift;
+    double* pwrite   = write.data();
+    const double* pread   = read.data() + shift;
+    double* end = pwrite + write.size();
+    double* block;
+    while (pwrite < end) {
+        block = pwrite + unshift;
+        while (pwrite < block) {*pwrite++ += *pread++;}
+        pread -= group;
+        block = pwrite + shift;
+        while (pwrite < block) {*pwrite++ += *pread++;}
+        pread += group;
+    }
+}
+
 vector<double> SHN_PSA(const vector<double>& space, const vector<int>& q, int r){
     //restrictions
-    int len_q = q.size();
-    int len_s = space.size();
-    int prod_q = 1;
-    for (int val : q){prod_q *= val;}
-    if (len_s != prod_q){throw invalid_argument("len(space) == prod(q)");}
-    if (len_q < r){throw invalid_argument("0 <= r <= len(q)");}
+    int prod_q = 1; for (int val : q){prod_q*=val;}
+    if (space.size() != prod_q){throw invalid_argument("len(space) == prod(q)");}
+    if (q.size() < r){throw invalid_argument("0 <= r <= len(q)");}
     for (int val : q){if (val<=1) throw invalid_argument("q[i]>1"); }
     //getbase(q)
-    vector<vector<int>> base(len_q);
+    vector<vector<int>> base(q.size());
     int cumprod=1;
-    for(int i = len_q - 1; i >= 0; --i){
-        base[i].resize(q[i]);
-        for(int n = q[i], j = 0; n > 0; --n, ++j){base[i][j]=n*cumprod;}
+    for(int i = q.size() - 1; i >= 0; --i){
+        base[q.size()-1-i].resize(q[i]);
+        for(int n = q[i], j = 0; n > 0; --n, ++j){base[q.size()-1-i][j]=n*cumprod;}
         cumprod*=q[i];
     }
     //PSA
     vector<double> sum = space;
-    vector<double> aux(len_s);
-    vector<vector<double>> acum(len_q, space);
-    for (int n=0; n<r; ++n) {
+    vector<double> aux(space.size());
+    vector<vector<double>> acum(q.size(), space);
+    for(int n=0; n<r; ++n){
         fill(aux.begin(), aux.end(), 0.0);
-        for (int i=n; i<len_q; ++i) {
-            //basic transformation, same result as numpy functions reshape+roll+flatten
-            //TODO: take non-dependent variables out of deep loops
-            int t = 0;
-            vector<int> indices(base[i].begin()+1, base[i].end());
-            while (t<len_s) {
-                for (int k=0; k<indices.size(); indices[k]-=base[i][0], k++) {
-                    for (int j=0; j<base[i].back(); ++j, t++) {
-                        for (int idx=0; idx<indices.size(); indices[idx]++, ++idx){aux[t]+=acum[i-n][indices[idx]];}
-                    }
-                }
-                for (int j=0; j<base[i].back(); ++j, t++) {
-                    for (int idx=0; idx<indices.size(); indices[idx]++, ++idx){aux[t]+=acum[i-n][indices[idx]];}
-                }
-                for(int &index:indices){index += base[i][0];}
+        for(int i=n; i<q.size(); ++i){
+            for(int k=1; k<base[i].size(); ++k){
+                apply_diff(base[i][k], base[i][0], acum[i-n], aux);
             }
             acum[i-n]=aux;
         }
-        for (size_t j=0; j<sum.size(); ++j){sum[j]+=acum[len_q-n-1][j];}
+        for(size_t j=0; j<space.size(); ++j){sum[j]+=aux[j];}
     }
     return sum;
 }
 
 int main(){
+    //inputs
     int r=4;
     vector<int> q = {3,3,3,3,3,3,3,3,3,3,3,3,3,3};
-    //vector<int> q = {2,3};
+    //vector<int> q = {2,2,2,2,2,10000};
+    //vector<int> q = {10000,2,2,2,2,2};
     int prod_q = 1;
-    for (int val : q){prod_q *= val;}
-    //create space
+    for (int val : q){prod_q*=val;}
     vector<double> space(prod_q, 0.0);
-    for (int i=0; i<prod_q; ++i) {space[i]=i;}
-
+    for (int i=0; i<prod_q; ++i){space[i]=i;}
+    
+    //output
     vector<double> shn = SHN_PSA(space, q, r);
-    for (int i = 0; i < 10; ++i) {
-        cout << static_cast<long long>(shn[i]) << endl;
-    }
-
-    //TODO: implement function for symMetric space
+    for (int i = 0; i < 10; ++i){cout << static_cast<long long>(shn[i]) << endl;}
 
     /*PRINT
     18847285404
@@ -78,6 +78,5 @@ int main(){
     18847376924
     18847388364
     */
-
     return 0;
 }
