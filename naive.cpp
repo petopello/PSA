57e3efe2010cfc285b0b6b97b9f33e5cb596c32b
petopello <52197415+petopello@users.noreply.github.com>
2025-03-06 13:43:29 +0100

    Update naive.cpp

--- naive.cpp
+++ naive.cpp
@@ -28,24 +28,22 @@ double SHN_naive(int index, array<int, DIM> coords, const vector<double>& space,
 
 int main() {
     //create space
-    std::vector<double> space(SIZE, 0.0);
+    vector<double> space(SIZE, 0.0);
     for (int i = 0; i < SIZE; ++i) {
         space[i] = i;
     }
-
     //create base
-    std::vector<std::vector<int>> base(DIM, std::vector<int>(4));
+    vector<vector<int>> base(DIM, vector<int>(4));
     for (int i = 0; i < DIM; ++i) {
         int p = pow(3, DIM-i-1);
         base[i] = {p * 2, p * 1, p * -1, p * -2};
     }
-
     //loop for call SHN_naive with each point in space
     double value_naive = 0;
-    std::array<int, DIM> coords = {};
+    array<int, DIM> coords = {};
     for (int index = 0; index < 10; ++index) {
         value_naive = SHN_naive(index, coords, space, RADIUS, base);
-        std::cout << static_cast<long long>(value_naive) << std::endl;
+        cout << static_cast<long long>(value_naive) << endl;
         // update coords
         for (int i = DIM-1; i >= 0; --i) {
             if (++coords[i] < 3) break;
